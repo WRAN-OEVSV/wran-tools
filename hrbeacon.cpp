2ba0b048119444a6c02604f13952fb4cd9d43322
Roland Schwarz OE1RSA <roland.schwarz@blackspace.at>
2023-12-30 15:34:56 +0100

    added lot of experimental stuff

---
+++ hrbeacon.cpp
@@ -0,0 +1,390 @@
+﻿/*
+ * SPDX-FileCopyrightText: 2023 Roland Schwarz <roland.schwarz@blackspace.at>
+ * SPDX-License-Identifier: GPL-3.0-or-later
+*/
+
+/*
+ * A tool for generating a beacon signal based on the work of Marek Honek
+ * in his master thesis: SDR OFDM Frame Generation according to IEEE 802.22.
+ * https://doi.org/10.34726/hss.2022.74390, but heavily adapted by OE1RSA.
+ */
+
+#include "config.hpp"
+#include "hamranfrm.hpp"
+
+#include <cxxopts.hpp>
+
+#include <lime/LimeSuite.h>
+#include <lime/Logger.h>
+
+#include <complex> // NB: Must be included before liquid.h !
+#include <liquid/liquid.h>
+
+#include <cstdlib>
+using std::size_t;
+
+#include <iostream>
+using std::cout, std::cerr, std::clog, std::cin, std::endl;
+
+#include <stdexcept>
+using std::runtime_error;
+
+#include <exception>
+using std::exception;
+
+#include <map>
+using std::map;
+
+#include <initializer_list>
+using std::initializer_list;
+
+#include <complex>
+using std::complex, std::abs;
+using namespace std::literals::complex_literals;
+
+#include <algorithm>
+using std::max;
+
+#include <limits>
+using std::numeric_limits;
+
+#include <csignal>
+using std::sig_atomic_t, std::signal;
+
+#include <thread>
+using std::jthread, std::stop_token, std::this_thread::sleep_for;
+
+#include <mutex>
+using std::mutex;
+
+#include <condition_variable>
+using std::condition_variable;
+
+#include <atomic>
+using std::atomic;
+
+#include <string>
+using std::string, std::getline;
+
+#include <vector>
+using std::vector;
+
+#include <functional>
+using std::ref;
+
+#include <chrono>
+using std::chrono::high_resolution_clock, std::chrono::duration_cast,
+  std::chrono::nanoseconds;
+using namespace std::literals::chrono_literals;
+
+#include <boost/format.hpp>
+using boost::format, boost::str;
+
+namespace {
+
+  const lime::LogLevel default_LogLevel = lime::LOG_LEVEL_INFO;
+  lime::LogLevel max_LogLevel = default_LogLevel;
+
+  void limeSuiteLogHandler(const lime::LogLevel level, const char* message) {
+    if (level <= max_LogLevel) {
+        switch(level) {
+          //rem: throwing on error avoids overly verbose error handling code
+          case lime::LOG_LEVEL_CRITICAL: throw runtime_error(message);
+          case lime::LOG_LEVEL_ERROR:    throw runtime_error(message);
+          case lime::LOG_LEVEL_WARNING: clog << format("Warning: %s\n") % message; return;
+          case lime::LOG_LEVEL_INFO:    clog << format("Info: %s\n") % message; return;
+          case lime::LOG_LEVEL_DEBUG:   clog << format("Debug: %s\n") % message; return;
+          }
+      }
+  }
+
+  volatile sig_atomic_t signal_status = 0;
+  void signal_handler(int signal) {
+    signal_status = signal;
+  }
+
+}
+
+const double sample_rate = 4e6; // lime specific
+
+// some global shared variables for use by the threads
+atomic<uint64_t> rx_timestamp      = 0; // latest timestamp from rx, we know of
+atomic<size_t>   cpf               = 0; // cyclic prefix len, 0 ... prefix_divider
+atomic<size_t>   phy_mode          = 1; // physical layer mode;
+
+// this is a hack, proper implementation pending
+string global_message;
+
+// Note: LMS_SendStream and LMS_RecvStream are the ONLY thread safe functions
+// of limesuite.
+// The transmit thread:
+void transmit(stop_token stoken, lms_stream_t& tx_stream, uint64_t start_time) {
+
+  float sample_max = numeric_limits<float>::min();
+
+  string message = global_message; // I know this is unproteced
+  unsigned char header[8] = {0,0,0,0,0,0,0,0};
+
+  hrframegen fg(cpf, phy_mode);
+  uint32_t samp_per_frame = sample_rate*hrframegen::frame_len;
+
+  vector<complex<float>> wr_buffer(fg.subcarriers + fg.prefix_len);
+  vector<complex<float>> tx_buffer(wr_buffer.size()*sample_rate/fg.sample_rate);
+  rresamp_cccf rs = rresamp_cccf_create_default(tx_buffer.size(), wr_buffer.size());
+  rresamp_cccf_set_scale(rs, 0.245f);
+
+  uint64_t tx_timestamp = start_time;
+  lms_stream_meta_t meta;
+
+  while(!stoken.stop_requested()) {
+      tx_timestamp += samp_per_frame;
+      while(not stoken.stop_requested()
+            and rx_timestamp + samp_per_frame < tx_timestamp) {
+          sleep_for(10us); // In a bidirectionional setting waiting here could
+                           // be replaced by transmit data preparation.
+        }
+
+      if (not stoken.stop_requested()) {
+          fg.assemble(header, reinterpret_cast<unsigned char*>(message.data()), message.size());
+          bool last = fg.write(wr_buffer.data(), wr_buffer.size());
+          meta.timestamp = tx_timestamp;
+          meta.waitForTimestamp = true;
+          while (not last) {
+              rresamp_cccf_execute(rs, wr_buffer.data(), tx_buffer.data());
+              for (size_t i=0; i<tx_buffer.size(); ++i) sample_max = max(sample_max, abs(tx_buffer[i]));
+              LMS_SendStream(&tx_stream, tx_buffer.data(), tx_buffer.size(), &meta, 1000);
+              // TODO: I do not yet understand the write function completely.
+              // The documentaion and source of liquid appear incomplete.
+              last = fg.write(wr_buffer.data(), wr_buffer.size());
+              meta.waitForTimestamp = false;
+              meta.flushPartialPacket = false;
+            }
+          meta.flushPartialPacket = true;
+          rresamp_cccf_execute(rs, wr_buffer.data(), tx_buffer.data());
+          for (size_t i=0; i<tx_buffer.size(); ++i) sample_max = max(sample_max, abs(tx_buffer[i]));
+          LMS_SendStream(&tx_stream, tx_buffer.data(), tx_buffer.size(), &meta, 1000);
+        }
+    }
+
+  cout << "sample_max = " << sample_max << ", 1/sample_max = " << 1/sample_max << endl;
+  cout << "Transmit thread stopping." << endl;
+}
+
+// The receive thread:
+void receive(stop_token stoken, lms_stream_t& rx_stream, lms_stream_t& tx_stream) {
+  vector<complex<float>> rx_buffer(1024*4);
+  lms_stream_meta_t meta;
+  int ret = LMS_RecvStream(&rx_stream, rx_buffer.data(), rx_buffer.size(), &meta, 1000);
+  jthread tx_thread(transmit, ref(tx_stream), meta.timestamp);
+  rx_timestamp = meta.timestamp +  ret;
+
+  while(!stoken.stop_requested()) {
+      ret = LMS_RecvStream(&rx_stream, rx_buffer.data(), rx_buffer.size(), &meta, 1000);
+      rx_timestamp = meta.timestamp + ret;
+      // The receive thread does not do much yet, but fetch samples and update time.
+  }
+
+  cout << "Receive thread stopping."  << endl;
+
+  tx_thread.request_stop();
+  tx_thread.join();
+}
+
+// The control thread.
+int main(int argc, char* argv[])
+{
+
+  signal(SIGINT, signal_handler); // install handler to catch ctrl-c
+
+  max_LogLevel = lime::LOG_LEVEL_INFO;
+  lime::registerLogHandler(&limeSuiteLogHandler);
+
+  lms_device_t* dev = nullptr;
+
+  try {
+
+    cxxopts::Options options("wrbeacon", "Beacon generator for WRAN project.");
+    options.add_options()
+        ("h,help", "Print usage information.")
+        ("version", "Print version.")
+        ("freq", "Center frequency.", cxxopts::value<double>()->default_value("53e6"))
+        ("txpwr", "Tx Pwr. in in dBm (-26dBm ... 10dBm)", cxxopts::value<int>()->default_value("0"))
+        ("cpf", "Cyclic prefix len", cxxopts::value<size_t>()->default_value(("1")))
+        ("phy", "Physical layer mode: 1 ... 14", cxxopts::value<size_t>()->default_value("2"))
+        ("message", "Beacon message", cxxopts::value<string>()->default_value("OE1XTU"))
+        ;
+    options.parse_positional({"message"});
+    options.positional_help("message");
+    options.show_positional_help();
+
+    auto vm = options.parse(argc, argv);
+
+    if (vm.count("help")) {
+        cout << options.help() << endl;
+        return EXIT_SUCCESS;
+    }
+
+    if (vm.count("version")) {
+        cout <<PROJECT_VER << endl;
+        return EXIT_SUCCESS;
+    }
+
+    // copy to global variable, this is preliminary and will be replaced
+    // TODO: replace by message queue
+    global_message = vm["message"].as<string>();
+
+    cpf = vm["cpf"].as<size_t>();
+    if (0 == cpf or  cpf > hrframegen::prefix_divider)
+      throw runtime_error("prefix not in range");
+
+    phy_mode = vm["phy"].as<size_t>();
+    if (0 == phy_mode or phy_mode > 14)
+      throw runtime_error("invalid phymode requested");
+
+    //double band_width = 2.0e6;
+    double freq       = vm["freq"].as<double>();
+    double txpwr      = vm["txpwr"].as<int>();
+
+    cout << "wrbeacon. Version " PROJECT_VER << endl;
+
+    cout << "Parameters:" << endl;
+    cout << format("Freq:       %g MHz\n") % (1e-6*freq);
+    cout << format("Peak Pwr:   %g dBm\n") % vm["txpwr"].as<int>();
+    cout << format("Samplerate: %g MHz\n") % (1e-6*sample_rate);
+    cout << format("Prefix len: %2d\n")    % cpf;
+    cout << format("Phymode:    %2d\n")    % phy_mode;
+    cout << endl;
+
+    // Try to open devices until one is available or fail if none.
+    lms_info_str_t list[8];
+    int numdev = LMS_GetDeviceList(list);
+    if (numdev < 1) lime::error("No device found");
+    int n = 0;
+    for (; n < numdev; ++n) {
+        try {
+          LMS_Open(&dev, list[n], nullptr);
+          lime::info("Device: %s", list[n]);
+          break;
+        } catch(runtime_error& e) { /* possibly busy, try next one */}
+      }
+    if (n == numdev)
+      lime::error("No device could be obened");
+
+    LMS_Init(dev);
+
+    // Set up GPIO.
+    uint8_t gpio_dir = 0xFF;
+    LMS_GPIODirWrite(dev, &gpio_dir, 1);
+
+    uint8_t gpio_val = 0;
+    LMS_GPIODirRead(dev, &gpio_val, 1);
+    lime::debug("GPIODIR: 0x%02x", unsigned(gpio_val));
+
+    gpio_val = 0xb3; // 0xbf; //0xb3;
+    LMS_GPIOWrite(dev, &gpio_val, 1);
+
+// This is the fast, data synchronous TX/TX signaling
+#if 0
+    // Set up TX indicator on GPIO0.
+    // https://github.com/myriadrf/LimeSDR-Mini-v2_GW/issues/3
+    uint16_t fpga_val = 0;
+    LMS_ReadFPGAReg(dev, 0x00c0, &fpga_val);
+    fpga_val &= 0xfffe;
+    LMS_WriteFPGAReg(dev, 0x00c0, fpga_val);
+
+    // TXANT_PRE
+    fpga_val = static_cast<uint16_t>(10.5e-6*sample_rate);
+    LMS_WriteFPGAReg(dev, 0x0010, fpga_val);
+
+    // TXANT_POST
+    fpga_val = static_cast<uint16_t>(11.5e-6*sample_rate);
+    LMS_WriteFPGAReg(dev, 0x0011, fpga_val);
+#endif
+
+    LMS_SetSampleRate(dev,     sample_rate, 0);
+
+    // initialize receive direction
+    LMS_EnableChannel(dev,     LMS_CH_RX, 0, true);
+    LMS_SetAntenna(dev,        LMS_CH_RX, 0, LMS_PATH_LNAW);
+    LMS_SetLOFrequency(dev,    LMS_CH_RX, 0, freq);
+    LMS_SetNormalizedGain(dev, LMS_CH_RX, 0, 1.0);
+    LMS_SetGFIRLPF(dev,        LMS_CH_RX, 0, true, 1.86e6);
+    LMS_SetLPFBW(dev,          LMS_CH_RX, 0, 5e6);
+
+    lms_stream_t rx_stream;
+    rx_stream.channel = 0;
+    rx_stream.fifoSize = 1024;
+    rx_stream.throughputVsLatency = 0.5;
+    rx_stream.dataFmt = lms_stream_t::LMS_FMT_F32;
+    rx_stream.isTx = false;
+
+    // initialize transmit direction
+    LMS_EnableChannel(dev,     LMS_CH_TX, 0, true);
+    LMS_SetLOFrequency(dev,    LMS_CH_TX, 0, freq);
+    LMS_SetAntenna(dev,        LMS_CH_TX, 0, LMS_PATH_TX2);
+    LMS_SetGaindB(dev,         LMS_CH_TX, 0, 61+txpwr);
+    LMS_SetGFIRLPF(dev,        LMS_CH_TX, 0, true, 1.86e6);
+    LMS_SetLPFBW(dev,          LMS_CH_TX, 0, 5e6);
+
+    lms_stream_t tx_stream;
+    tx_stream.channel = 0;
+    tx_stream.fifoSize = 256*1024;
+    tx_stream.throughputVsLatency = 0.5;
+    assert(8*sizeof(float) == 32);
+    tx_stream.dataFmt = lms_stream_t::LMS_FMT_F32;
+    tx_stream.isTx = true;
+
+    LMS_Calibrate(dev,         LMS_CH_RX, 0, 5e6, 0);
+    LMS_Calibrate(dev,         LMS_CH_TX, 0, 5e6, 0);
+
+    LMS_SetupStream(dev, &rx_stream);
+    LMS_SetupStream(dev, &tx_stream);
+
+    LMS_StartStream(&rx_stream);
+    LMS_StartStream(&tx_stream);
+
+    // start the threads
+    jthread rx_thread(receive, ref(rx_stream), ref(tx_stream));
+    // transmit is started from inside receive thread
+
+    cout << "Beacon control thread: enter EOF (Ctrl-D) or empty line to end." << endl;
+    string line;
+    while(getline(cin, line)) {
+        if (line.empty()) break;
+    }
+    cout << "Exiting ..." << endl;
+
+    rx_thread.request_stop();
+    rx_thread.join();
+
+    cout << "Cleaning up ..." << endl;
+
+    LMS_StopStream(&tx_stream);
+    LMS_DestroyStream(dev, &tx_stream);
+    LMS_EnableChannel(dev, LMS_CH_TX, 0, false);
+
+    LMS_StopStream(&rx_stream);
+    LMS_DestroyStream(dev, &rx_stream);
+    LMS_EnableChannel(dev, LMS_CH_RX, 0, false);
+
+    gpio_val = 0x00;
+    LMS_GPIOWrite(dev, &gpio_val, 1);
+
+    LMS_Close(dev);
+    dev = nullptr;
+
+    cout << "Control thread stopping." << endl;
+
+  } catch(exception& e) {
+    cerr << "Exception: " << e.what() << endl;
+    if (nullptr != dev)
+      LMS_Close(dev);
+    return EXIT_FAILURE;
+  } catch(...) {
+    cerr << "Exception of unknown reason." << endl;
+    if (nullptr != dev)
+      LMS_Close(dev);
+    return EXIT_FAILURE;
+  }
+    return EXIT_SUCCESS;
+}
---
+++ wranfrm.cpp
@@ -0,0 +1,213 @@
+/*
+ * SPDX-FileCopyrightText: 2023 Roland Schwarz <roland.schwarz@blackspace.at>
+ * SPDX-License-Identifier: GPL-3.0-or-later
+*/
+
+#include <iostream>
+using std::cout, std::endl;
+
+#include <boost/format.hpp>
+using boost::format;
+
+#include <string>
+using std::string;
+
+#include "wranfrm.hpp"
+
+#include <cstddef>
+using std::size_t;
+
+#include <complex>
+using std::complex, std::abs;
+using namespace std::literals::complex_literals;
+
+#include <algorithm>
+using std::min, std::max, std::minmax;
+
+#include <cmath>
+using std::fmod;
+
+#include <limits>
+using std::numeric_limits;
+
+#include <stdexcept>
+using std::runtime_error;
+
+#include <cassert>
+
+const double wrframe::frame_len       =    10e-3; // s
+const double wrframe::bandwidth       = 2e6*0.92; // Hz
+const double wrframe::carrier_spacing =      4e3; // Hz
+const size_t wrframe::prefix_divider  =       50;
+const size_t wrframe::pilot_fraction  =        5;
+
+const wrframegen::phy_t wrframegen::phy_prop[] =
+{
+  {LIQUID_MODEM_PSK2,    LIQUID_FEC_NONE,        LIQUID_FEC_NONE},    //  1
+  {LIQUID_MODEM_QPSK,    LIQUID_FEC_CONV_V27,    LIQUID_FEC_REP3},    //  2
+  {LIQUID_MODEM_QPSK,    LIQUID_FEC_CONV_V27,    LIQUID_FEC_NONE},    //  3
+  {LIQUID_MODEM_QPSK,    LIQUID_FEC_CONV_V27P23, LIQUID_FEC_NONE},    //  4
+  {LIQUID_MODEM_QPSK,    LIQUID_FEC_CONV_V27P23, LIQUID_FEC_NONE},    //  5
+  {LIQUID_MODEM_QPSK,    LIQUID_FEC_CONV_V27P56, LIQUID_FEC_NONE},    //  6
+  {LIQUID_MODEM_QAM16,   LIQUID_FEC_CONV_V27,    LIQUID_FEC_NONE},    //  7
+  {LIQUID_MODEM_QAM16,   LIQUID_FEC_CONV_V27P23, LIQUID_FEC_NONE},    //  8
+  {LIQUID_MODEM_QAM16,   LIQUID_FEC_CONV_V27P34, LIQUID_FEC_NONE},    //  9
+  {LIQUID_MODEM_QAM16,   LIQUID_FEC_CONV_V27P56, LIQUID_FEC_NONE},    // 10
+  {LIQUID_MODEM_QAM64,   LIQUID_FEC_CONV_V27,    LIQUID_FEC_NONE},    // 11
+  {LIQUID_MODEM_QAM64,   LIQUID_FEC_CONV_V27P23, LIQUID_FEC_NONE},    // 12
+  {LIQUID_MODEM_QAM64,   LIQUID_FEC_CONV_V27P34, LIQUID_FEC_NONE},    // 13
+  {LIQUID_MODEM_QAM64,   LIQUID_FEC_CONV_V27P56, LIQUID_FEC_NONE}     // 14
+};
+
+wrframe::wrframe(double sample_rate, size_t prefix_fraction)
+  : sample_rate(sample_rate),
+    subcarriers(sample_rate/carrier_spacing),
+    available_carriers(bandwidth/carrier_spacing),
+    guards((subcarriers - available_carriers)/2),
+    used_carriers(available_carriers - available_carriers/pilot_fraction),
+    prefix_len((subcarriers*prefix_fraction)/prefix_divider),
+    taper_len(0) {//prefix_len/16) {
+
+  assert(0 == fmod(bandwidth, carrier_spacing));
+  assert(0 == fmod(subcarriers, 2));
+  assert(1 <= frame_len*carrier_spacing);
+
+  if (sample_rate < bandwidth)
+    throw runtime_error("sample rate must be larger than bandwidth");
+
+  if (0 != fmod(sample_rate, carrier_spacing*prefix_divider))
+    throw runtime_error("sample_rate must be multiple of carrier_spacing*prefix_divider");
+
+  sca.resize(subcarriers);
+  for (size_t i = 0;                  i < guards-1;             ++i)
+      sca[(i+subcarriers/2) % subcarriers] = OFDMFRAME_SCTYPE_NULL; // guard band
+
+  for (size_t i = guards;             i < subcarriers/2; ++i)
+      sca[((i+subcarriers/2) % subcarriers) - 1] = (0 == i%pilot_fraction)? OFDMFRAME_SCTYPE_PILOT :  OFDMFRAME_SCTYPE_DATA;
+
+  for (size_t i = subcarriers/2; i < subcarriers/2+2; ++i)
+    sca[(i+subcarriers/2) % subcarriers] = OFDMFRAME_SCTYPE_NULL; // DC block
+
+  for (size_t i = subcarriers/2;      i < subcarriers-guards; ++i)
+      sca[((i+subcarriers/2) % subcarriers) + 1] = (0 == i%pilot_fraction)? OFDMFRAME_SCTYPE_PILOT :  OFDMFRAME_SCTYPE_DATA;
+
+  for (size_t i = subcarriers-guards+1; i < subcarriers;        ++i)
+      sca[(i+subcarriers/2) % subcarriers] = OFDMFRAME_SCTYPE_NULL; // guard_band]
+
+//  for (size_t i = 0;                  i < guards;             ++i)
+//      sca[(i+subcarriers/2) % subcarriers] = OFDMFRAME_SCTYPE_NULL; // guard band
+
+//  for (size_t i = guards;             i < subcarriers-guards; ++i)
+//      sca[(i+subcarriers/2) % subcarriers] = (0 == i%pilot_fraction)? OFDMFRAME_SCTYPE_PILOT :  OFDMFRAME_SCTYPE_DATA;
+
+//  for (size_t i = subcarriers-guards; i < subcarriers;        ++i)
+//      sca[(i+subcarriers/2) % subcarriers] = OFDMFRAME_SCTYPE_NULL; // guard_band]
+
+}
+
+wrframe::~wrframe() {
+
+}
+
+wrframegen::wrframegen(double sample_rate, size_t prefix_fraction, size_t phy_mode)
+  : wrframe(sample_rate, prefix_fraction) {
+
+  ofdmflexframegenprops_s fgp;
+  ofdmflexframegenprops_init_default(&fgp);
+
+  fgp.check = LIQUID_CRC_32;
+  fgp.mod_scheme = phy_prop[phy_mode-1].mod_scheme;
+  fgp.fec0       = phy_prop[phy_mode-1].fec0;
+  fgp.fec1       = phy_prop[phy_mode-1].fec1;
+
+  fg = ofdmflexframegen_create(subcarriers, prefix_len, taper_len, sca.data(), &fgp);
+  ofdmflexframegen_print(fg);
+  ofdmframe_print_sctype(sca.data(), subcarriers);
+  sample_max = numeric_limits<float>::min();
+}
+
+void wrframegen::assemble(const unsigned char* header, const unsigned char* payload, size_t payload_len) {
+  ofdmflexframegen_assemble(fg, header, payload, payload_len);
+  //ofdmflexframegen_print(fg);
+}
+
+bool wrframegen::write(complex<float>* buffer, size_t buffer_len) {
+  int result = ofdmflexframegen_write(fg, buffer, buffer_len);
+  // Amplitudes > 1.0 overdrive the ADC resulting in splatter.
+  // TODO: The optimum scale factor needs to be determined.
+  for (size_t i=0; i < buffer_len; ++i) {
+      sample_max = max(sample_max, abs(buffer[i]));
+      buffer[i] *= 0.241; //0.3;//0.296;
+    }
+  return (1 == result)?true:false;
+}
+
+wrframegen::~wrframegen() {
+  ofdmflexframegen_destroy(fg);
+  cout << "sample_max = " << sample_max << ", 1/sample_max = " << 1/sample_max << endl;
+}
+
+extern "C" {
+  int hrframesync_wrap_callback(unsigned char* _header,
+                int _header_valid,
+                unsigned char* _payload,
+                unsigned int _payload_len,
+                int _payload_valid,
+                framesyncstats_s _stats,
+                void* _userdata) {
+    return static_cast<wrframesync*>(_userdata)->callback(
+          _header, _header_valid, _payload, _payload_len, _payload_valid,
+          _stats);
+    return 0;
+  }
+}
+
+wrframesync::wrframesync(double sample_rate, size_t prefix_fraction)
+  : wrframe(sample_rate, prefix_fraction) {
+  fs = ofdmflexframesync_create(subcarriers, prefix_len, taper_len,
+                                sca.data(), hrframesync_wrap_callback, this);
+  ofdmflexframesync_print(fs);
+}
+
+wrframesync::~wrframesync() {
+  ofdmflexframesync_print(fs);
+  ofdmflexframesync_destroy(fs);
+}
+
+int wrframesync::callback(unsigned char* header, bool header_valid,
+             unsigned char* payload, unsigned int payload_len,
+             bool payload_valid, framesyncstats_s stats) {
+  cout << format("%6.1f, %6.1fe-6") % stats.rssi % (1e6*stats.cfo);
+  if (nullptr != payload) {
+    if (payload_valid)
+      cout << ", " << payload_len << ": " << string(reinterpret_cast<char*>(payload)).substr(0, payload_len);
+    else
+      cout << ", PAYLOAD INVALID";
+    }
+  else
+    cout << ", PAYLOAD MISSING";
+  cout << endl;
+  return 0;
+}
+
+bool wrframesync::execute(complex<float>* buffer, size_t buffer_len) {
+  ofdmflexframesync_execute(fs, buffer, buffer_len);
+  return true;
+}
+
+bool wrframesync::execute(complex<uint8_t>* buffer, size_t buffer_len) {
+  // convert 8 bit to complex float, as e.g. for rtl sdr
+  complex<float> fbuffer[buffer_len];
+  for (size_t n=0; n<buffer_len; ++n)
+      fbuffer[n] = (buffer[n].real() / 127.5 - 1) + 1i*(buffer[n].imag() / 127.5 -1 );
+  return execute(fbuffer, buffer_len);
+}
+
+bool wrframesync::execute(complex<int8_t>* buffer, size_t buffer_len) {
+  // convert 8 bit to complex float, as e.g. for hackrf
+  complex<float> fbuffer[buffer_len];
+  for (size_t n=0; n<buffer_len; ++n)
+      fbuffer[n] = (buffer[n].real() / 127.5) + 1i*(buffer[n].imag() / 127.5 );
+  return execute(fbuffer, buffer_len);
+}
+
